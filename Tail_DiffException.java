db4956ac208745089ac933c2b7f0bc3a18677221
Souad360 <32779871+Souad360@users.noreply.github.com>
2017-10-26 00:23:48 +0200

    exception
    gerer une exception lorsque la taille de deux tableaux a sommer sont de taille differantes

---
+++ Tail_DiffException.java
@@ -0,0 +1,16 @@
+
+public class Tail_DiffException extends Exception {
+
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * une methode qui affiche la raison de l exception lancer
+     */
+    public void tailleDiff(){
+
+        System.out.println("la taille des deux tableaux est différente  , on peux pas les sommer !!!! ");
+    }
+
+
+}
